24b89ed1ded2c0b79592995884f4204c17838446
pablohr15 <pablitoherreruela15@gmail.com>
2023-06-21 18:41:08 +0200

    Primer commit

---
+++ script.js
@@ -0,0 +1,29 @@
+fetch("https://sheets.googleapis.com/v4/spreadsheets/1m9hab2nw1GdqoLbvjJBe7vE8GSiWzliaTXcwzoYKugE/values/ringo-el-curioso?key=AIzaSyB9vIKNAaEYTQ2_Akh48VMdNioidhBIl-o")
+.then(res => res.json())
+.then(data => {
+
+    const datos = data.values
+    const rows = datos[1].length
+    const characters = document.getElementById('characters')
+
+    for (let i = 0; i < rows; i++) {
+        let character = {
+            characterImage: datos[0][i],
+            characterName: datos[1][i],
+            characterDescription: datos[2][i]
+        }
+        
+        characters.innerHTML += `  
+        <div class="card">
+            <img src="assets/${i}.jpeg"></img>
+            <div class="container">
+                <h4 style="font-size:20px"><b>${character.characterName}</b></h4>
+                <p>${character.characterDescription}</p>
+            </div>
+        </div>
+        `
+
+    }
+
+        
+})
