aa9d350f2e0c01e74c28b21f68ad596e3c1c2301
pablohr15 <pablitoherreruela15@gmail.com>
2023-06-22 21:35:45 +0200

    Commit White Fox

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 fetch("https://sheets.googleapis.com/v4/spreadsheets/1m9hab2nw1GdqoLbvjJBe7vE8GSiWzliaTXcwzoYKugE/values/ringo-el-curioso?key=AIzaSyB9vIKNAaEYTQ2_Akh48VMdNioidhBIl-o")
 .then(res => res.json())
 .then(data => {
@@ -12,7 +13,7 @@ fetch("https://sheets.googleapis.com/v4/spreadsheets/1m9hab2nw1GdqoLbvjJBe7vE8GS
             characterName: datos[1][i],
             characterDescription: datos[2][i]
         }
-        
+   
         characters.innerHTML += `  
         <div class="card">
             <img src="assets/${i}.jpeg"></img>
