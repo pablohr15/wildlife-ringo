e0b445e816fe44642a2be637939af023aeaa0827
pablohr15 <pablitoherreruela15@gmail.com>
2023-06-28 02:25:05 +0200

    Jueves 28 Junio - Nueva cabecera con estilos

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ fetch("https://sheets.googleapis.com/v4/spreadsheets/1m9hab2nw1GdqoLbvjJBe7vE8GS
     console.log(filters)
     let charactersList = [];
 
+    const categories_color = {
+        "Basketball Team": "rgb(248, 245, 51)",
+        "Kingdom Population": "rgb(70, 157, 255)",
+        "Ship's Company": "rgb(255, 69, 69)",
+        "Wild West": "rgb(132, 255, 50)",
+        "Summertime": "rgb(252, 147, 26)"
+    }
+
+
+
     for (let i = 0; i < rows; i++) {
         let character = {
             characterImage: datos[0][i],
@@ -18,10 +28,12 @@ fetch("https://sheets.googleapis.com/v4/spreadsheets/1m9hab2nw1GdqoLbvjJBe7vE8GS
         }
 
         charactersList.push(character);
+
+        console.log(categories_color[character.characterDescription])
    
         characters.innerHTML += `  
         <div class="character-card">
-            <img src="assets/${i}.jpeg"></img>
+            <img style="border-radius: 10px; width: 100%; height: auto; border: 2px solid rgb(255, 146, 250); box-shadow: 0 0 15px rgb(255, 146, 250), 0 0 30px white;" src="assets/${i}.jpeg"></img>
             <div class="container">
                 <h4 style="font-size:20px"><b>${character.characterName}</b></h4>
                 <p style="font-style:italic">${character.characterDescription}</p>
@@ -31,19 +43,31 @@ fetch("https://sheets.googleapis.com/v4/spreadsheets/1m9hab2nw1GdqoLbvjJBe7vE8GS
 
     }
 
+
+
     filters.forEach((filter) => {
         filter.addEventListener('click', () =>{
             
             $('.navbar-collapse').collapse('hide');
 
+            filters.forEach((filter) => {
+                filter.style.setProperty('text-shadow', 'none');
+            })
+
+            filter.style.setProperty('text-shadow', '0 0 30px white', 'important');
+
             characters.innerHTML = '';
+
+            let $head = document.getElementsByClassName("headname")[0];
+            $head.style.setProperty('color', `${categories_color[filter.innerText]}`, 'important');
+            console.log($head);
 
             for (let i = 0; i < rows; i++) {
                 let characterFromList = charactersList[i]
                 if (characterFromList["characterDescription"].includes(filter.innerText)){
                     characters.innerHTML += `  
                     <div class="character-card">
-                        <img src="assets/${i}.jpeg"></img>
+                        <img style="border-radius: 10px; width: 100%; height: auto; border: 2px solid ${categories_color[filter.innerText]}; box-shadow: 0 0 15px ${categories_color[filter.innerText]}, 0 0 30px white;" src="assets/${i}.jpeg"></img>
                         <div class="container">
                             <h4 style="font-size:20px"><b>${characterFromList.characterName}</b></h4>
                             <p style="font-style:italic">${characterFromList.characterDescription}</p>
